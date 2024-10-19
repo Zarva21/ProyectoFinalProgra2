cae7363041d7493932a497bd10b6e06398c58449
Zarva21 <165863841+Zarva21@users.noreply.github.com>
2024-10-19 04:27:55 -0600

    coneccion ya

--- app/config/env.js
+++ app/config/env.js
@@ -1,8 +1,8 @@
 const env = {
-    database: 'ProyectoFinalProgra2',
-    username: 'postgres',
-    password: '1234',
-    host: 'localhost',
+    database: 'proyectofinalprogra2',
+    username: 'proyectofinalprogra2_user',
+    password: 'YMckxQXqwD1kQrnH455Bd7wTBrHPcVCH',
+    host: 'dpg-cs9o1sa3esus739is6o0-a.oregon-postgres.render.com',
     port: '5432',
     dialect: 'postgres',
 
@@ -13,3 +13,5 @@ const env = {
         idle: 10000
     }
 };
+
+module.exports = env;
