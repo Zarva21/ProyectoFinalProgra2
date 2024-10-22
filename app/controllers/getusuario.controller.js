990802e2c1499aba0b50cb7b0325ccf7075b7d43
Zarva21 <165863841+Zarva21@users.noreply.github.com>
2024-10-21 20:40:43 -0600

    A hacer

---
+++ app/controllers/getusuario.controller.js
@@ -0,0 +1,25 @@
+
+
+/*
+conseguir el rol del usuario con la contrase;a y el usuario, primero validar si hay una respues de que 
+el nombre y el contrase;a sean iguales, para luego venir y jalar el el id del usaur y verificar el rrol atra vez de su id, 
+podria de una enontrarl atravez de dos campos encontrar el 3ro sin tenr que ingresar o meterme con el id solamente buscar el rol = nombre usuario 
+
+
+
+modificar usuarios y tipo de usuarios ya que ahora el id de usario sera un rol de l cual es int ya no sera algo del cuual 
+uno tendra la libertad de modificar de una vez o es uno o es el otro 
+
+
+USUARIOS	
+id usuario	primary key 
+id persona	int
+rol usuario	Boolean  ----- ya no se usara el rol ya que al final solamente sera entre uno o el otro para habilitar que sea admin  
+usuario 	Varchar(20)
+contrasena	Varchar(20)
+estado	Boolean
+
+
+
+
+*/
--- app/controllers/search.controller.js
+++ app/controllers/search.controller.js
@@ -84,3 +84,32 @@ exports.generalSearch = async (req, res) => {
     }
 };
 
+
+
+/*  
+la cosa es que si una persona en un formulario de cleinte, y quisiera dar el id del ciente,
+el id de cleinte no esta en persona, lo mismo en la talba de empleado que no podra encotnrar eso mismo ya que alf ianl en empleados y cliente son los 
+unicos que tengran ese extra en el ual estan relacionados a otra tabla de la cual tiene la informacion general
+
+PERSONAS 	        
+Id Persona	primary key 
+Nombre	Varchar(20)
+Apellido	Varchar(20)
+Fecha Nacimiento	Date
+Nit	varchar(20)
+Correo 	varchar(40)
+Genero	varchar(1)
+DPI	Int (20)
+Numero	int
+Estado	Boolean
+
+CLIENTES	
+Id Persona	primary key
+ID cliente	serial
+	
+EMPLEADOS	
+Id persona 	primary key 
+Id Empleado	serial 
+id tipo empleado	primary key 
+Activo	Boolean
+*/
